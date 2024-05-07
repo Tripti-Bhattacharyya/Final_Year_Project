58167f2691e861f41fb2927d09f12cc85309ef6f
Tripti Bhattacharyya <triptibh00@gmail.com>
2024-05-07 18:45:52 +0530

    modified some design at chat interface

--- Login_App/login_app/src/components/doctors/DoctorList.js
+++ Login_App/login_app/src/components/doctors/DoctorList.js
@@ -40,11 +40,12 @@ const DoctorList = () => {
     }
   };
 
+  
   return (
-    <div>
-      <h2>Available Doctors</h2>
-      {/* Search form */}
-      <form onSubmit={handleSearchSubmit}>
+    <div className='doc-container'>
+      <h2 className='doc-heading'>Available Doctors</h2>
+     
+      <form  className='search-bar' onSubmit={handleSearchSubmit}>
         <input
           type="text"
           placeholder="Search by name"
